97267e6824f170559816f7d29c3316ec870a88be
Olefirenko Oleg <espavoru@gmail.com>
2022-01-27 15:33:22 +0200

    Pricing section

---
+++ src/blocks/pricing/pricing.js
@@ -0,0 +1,17 @@
+/*global Swiper*/
+
+const swiper = new Swiper('.swiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+
+swiper.slideNext()
